535129cf4ea42154c7e8afc847a6a1381ab1f141
GraceCummings <cummingsge@vcu.edu>
2020-02-18 15:38:49 -0600

    updated condro production for new ttbar samples, reduced DYjets, and added signal configs

--- Production/test/condorSub/dict_Fall17_dyjets.py
+++ Production/test/condorSub/dict_Fall17_dyjets.py
@@ -11,7 +11,5 @@ flist = {
         ['Fall17.DYJetsToLL_M-50_HT-800to1200_TuneCP5_13TeV-madgraphMLM-pythia8'],
         ['Fall17.DYJetsToLL_M-50_HT-1200to2500_TuneCP5_13TeV-madgraphMLM-pythia8'],
         ['Fall17.DYJetsToLL_M-50_HT-2500toInf_TuneCP5_13TeV-madgraphMLM-pythia8'],
-        ['Fall17.DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8'],
-        ['Fall17.DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8_ext1'],
     ]
 }
