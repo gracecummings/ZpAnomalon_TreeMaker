456a92f6bc194334060fd5a13dae48faf9a9da3f
Grace Cummings <graceelainecummings@gmail.com>
2021-05-11 04:06:04 -0500

    updated dictionaries with ZpAnonalon appropriate datasets

--- Production/test/condorSub/dict_2018D_part1.py
+++ Production/test/condorSub/dict_2018D_part1.py
@@ -2,6 +2,7 @@ flist = {
     "scenario": "2018PromptReco",
     "samples": [
         ['Run2018D-22Jan2019-v2.EGamma'],
-        ['Run2018D-PromptReco-v2.MET'],
+        ['Run2018D-22Jan2019-v2.SingleMuon'],#gec added 29-04-21
+        #['Run2018D-PromptReco-v2.MET'],
     ]
 }
