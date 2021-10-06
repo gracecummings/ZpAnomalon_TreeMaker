10f5c5a8092e77e6cc81eddc9cc7732843b83cb7
Atanu Pathak <pathakatanu01@gmail.com>
2021-10-06 01:50:57 -0500

    adding similar cuts as B2G-19-006

--- Utils/src/ZProducer.cc
+++ Utils/src/ZProducer.cc
@@ -73,26 +73,29 @@ void ZProducer::produce(edm::StreamID, edm::Event& iEvent, const edm::EventSetup
    std::vector<size_t> subMuonIdx;//in the muons collection
 
    for (std::vector<pat::Muon>::const_iterator iL1 = muons->begin(); iL1 != muons->end(); ++iL1) {
-       if (iL1->pt() > 60.0) { // pat collection is pT-ordered
+       if (iL1->pt() > 60.0 && iL1->eta() < 2.4) { // pat collection is pT-ordered
           for (const auto & muon : *muons) {
-	       if (iL1->charge()*muon.charge() > 0) continue;
-	       mu1 = *(iL1->clone());
-	       mu2 = muon;
-	       aZ.setP4(mu1.p4()+muon.p4());
-	       if ((aZ.mass() <=110) && (aZ.mass()) >= 70){
-		  aZlist.push_back(aZ);
-		  leadMuons.push_back(mu1);
-		  subMuons.push_back(mu2);
-		  //throw exceptions
-		  //size_t leadIdx = std::distance(iL1,muons->begin());
-		  //std::cout<<"The lead muon pT is        "<<mu1.pt()<<std::endl;
-		  //std::cout<<"The lead muon pT at IDx is "<<muons->at(leadIdx).pt()<<std::endl;
-		  //std::cout<<"--------"<<std::endl;
-                  findZ = true;
-		  }
+	    if (muon.pt() > 20.0 && muon.eta() < 2.4){
+	      if (muon.pt() > iL1->pt()) continue;
+	      if (iL1->charge()*muon.charge() > 0) continue;
+	      mu1 = *(iL1->clone());
+	      mu2 = muon;
+	      aZ.setP4(mu1.p4()+muon.p4());
+	      if ((aZ.mass() <=110) && (aZ.mass()) >= 70){
+		aZlist.push_back(aZ);
+		leadMuons.push_back(mu1);
+		subMuons.push_back(mu2);
+		//throw exceptions
+		//size_t leadIdx = std::distance(iL1,muons->begin());
+		std::cout<<"The lead muon pT is        "<<mu1.pt()<<std::endl;
+		std::cout<<"The sublead muon pT is        "<<mu2.pt()<<std::endl;
+		//std::cout<<"The lead muon pT at IDx is "<<muons->at(leadIdx).pt()<<std::endl;
+		//std::cout<<"--------"<<std::endl;
+		findZ = true;
+	      }
+	    }
 	  }
-	  
-      }
+       }
    }
 
    //Ideas about how this could be better, but requires more input
@@ -157,18 +160,23 @@ void ZProducer::produce(edm::StreamID, edm::Event& iEvent, const edm::EventSetup
    std::list<pat::Electron> subElectrons;
 
    for (std::vector<pat::Electron>::const_iterator iL1 = electrons->begin(); iL1 != electrons->end(); ++iL1) {
-       if (iL1->pt() > 120.0) { // pat collection is pT-ordered
+       if (iL1->pt() > 60.0 && iL1->eta() < 2.4) { // pat collection is pT-ordered
           for (const auto & electron : *electrons) {
-               if (iL1->charge()*electron.charge() > 0) continue;
-               e1 = *(iL1->clone());
-               e2 = electron;
-               aZ.setP4(e1.p4()+e2.p4());
-               if ( aZ.mass() <= 110 && aZ.mass() >= 70){
-                  aZlist.push_back(aZ);
-                  leadElectrons.push_back(e1);
-                  subElectrons.push_back(e2);
-                  findZ = true;
-               }
+	    if (electron.pt() > 20.0 && electron.eta() < 2.4){
+	      if (electron.pt() > iL1->pt()) continue;
+	      if (iL1->charge()*electron.charge() > 0) continue;
+	      e1 = *(iL1->clone());
+	      e2 = electron;
+	      aZ.setP4(e1.p4()+e2.p4());
+	      if ( aZ.mass() <= 110 && aZ.mass() >= 70){
+		aZlist.push_back(aZ);
+		leadElectrons.push_back(e1);
+		subElectrons.push_back(e2);
+		std::cout<<"The lead electron pT is:  "<< e1.pt() << std::endl;
+		std::cout<<"The sublead electron pT:  "<< e2.pt() <<std::endl;
+		findZ = true;
+	      }
+	    }
           }
       }
    }
