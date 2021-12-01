2b92620c4a3d5a4b08fc0bc6c80a3b09da318f93
Grace Cummings <graceelainecummings@gmail.com>
2021-12-01 07:12:58 -0600

    additional dictionaries

---
+++ Production/test/condorSub/dict_re2017BC_lepton.py
@@ -0,0 +1,9 @@
+flist = {
+    "scenario": "2017ReReco31Mar",
+    "samples": [
+        ['Run2017B-31Mar2018-v1.SingleElectron'],
+        ['Run2017B-31Mar2018-v1.SingleMuon'],
+        ['Run2017C-31Mar2018-v1.SingleElectron'],
+        ['Run2017C-31Mar2018-v1.SingleMuon']
+    ]
+}
---
+++ Production/test/condorSub/dict_re2018ABC_lepton.py
@@ -0,0 +1,13 @@
+flist = {
+    "scenario": "2018ReReco17Sep",
+    "samples": [
+        ['Run2018A-17Sep2018-v1.EGamma'],
+        #['Run2018A-17Sep2018-v1.JetHT'],
+        #['Run2018A-17Sep2018-v1.MET'],
+        ['Run2018A-17Sep2018-v1.SingleMuon'],
+        ['Run2018B-17Sep2018-v1.EGamma'],
+        ['Run2018B-17Sep2018-v1.SingleMuon'],
+        ['Run2018C-17Sep2018-v1.EGamma'],
+        ['Run2018C-17Sep2018-v1.SingleMuon'],
+    ]
+}
