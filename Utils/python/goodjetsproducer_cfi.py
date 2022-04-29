011cf4ff85e492f86f762dc75b5c8973793a734a
Grace Cummings <graceelainecummings@gmail.com>
2022-04-29 11:20:19 -0500

    updated the 2016 jet ID to tight

--- Utils/python/goodjetsproducer_cfi.py
+++ Utils/python/goodjetsproducer_cfi.py
@@ -18,7 +18,7 @@ GoodJetsProducer = cms.EDFilter('GoodJetsProducer',
     etamin    = cms.vdouble( -1.0, -1.0,-1.0, -1.0,-1.0, -1.0,  2.7,  2.7, 2.7,  3.0, 3.0),
     etamax    = cms.vdouble(  2.7,  2.7, 2.7,  2.4, 2.4,  2.4,  3.0,  3.0, 3.0, -1.0,-1.0),
     cutvalmin = cms.vdouble( -1.0, -1.0, 1.0,  0.0, 0.0, -1.0, 0.01, -1.0, 2.0, -1.0,10.0),
-    cutvalmax = cms.vdouble( 0.99, 0.99,-1.0, -1.0,-1.0, 0.99, -1.0, 0.98,-1.0, 0.90,-1.0),
+    cutvalmax = cms.vdouble( 0.9, 0.9,-1.0, -1.0,-1.0, 0.99, -1.0, 0.98,-1.0, 0.90,-1.0),
 
     jetPtFilter = cms.double(30.),
     invertJetPtFilter = cms.bool(False),
